4ebc48dbdf255188f6d5dadd09966006b7322f29
LuisVeraVR <cocicor.luis.vera@gmail.com>
2025-05-20 22:12:17 -0500

    feat: implementar inicialización de base de datos y manejar solicitudes en index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
+
+
+import "reflect-metadata";
+import { AppDataSource } from "./src/config/database";
 import app from './src/app';
 
-export default app;
+let isDbInitialized = false;
+
+const initializeDb = async () => {
+  if (!isDbInitialized) {
+    try {
+      await AppDataSource.initialize();
+      console.log("Database has been initialized!");
+      isDbInitialized = true;
+    } catch (error) {
+      console.error("Error during Data Source initialization:", error);
+    }
+  }
+};
+
+const handler = async (req, res) => {
+  await initializeDb();
+  
+  return app(req, res);
+};
+
+export default handler;
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,19 @@
-
 import { DataSource } from "typeorm";
 import { Writer } from "../entity/writers";
 import { Book } from "../entity/book";
+import * as path from "path";
+
+const isProd = process.env.NODE_ENV === "production";
+const dbPath = isProd 
+  ? path.join("/tmp", "db.sqlite") 
+  : "db.sqlite";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "db.sqlite",
-  synchronize: true, 
-  logging: false, 
+  database: dbPath,
+  synchronize: true,
+  logging: false,
   entities: [Writer, Book],
+  migrations: [],
+  subscribers: []
 });
